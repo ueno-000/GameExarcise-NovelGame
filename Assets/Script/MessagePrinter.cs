ea6460b66f213a05e81a67e7a325f69da5dfe9d2
ueno-000 <sarasa4869@zoho.com>
2022-10-23 23:46:47 +0900

    [Update] Textを一文字ずつ表示する

--- Assets/Script/MessagePrinter.cs
+++ Assets/Script/MessagePrinter.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class MessagePrinter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] 
+    private TMP_Text _textUi = default;
+
+    [SerializeField]
+    private float _speed = 1f;
+
+    [SerializeField]
+    private string _message = "";
+
+
+    private float _elapsed = 0; // 文字を表示してからの経過時間
+    private float _interval; // 文字毎の待ち時間
+
+    // _message フィールドから表示する現在の文字インデックス。
+    // 何も指していない場合は -1 とする。
+    private int _currentIndex = -1;
+
+    private void Start()
     {
-        
+        if (_textUi is null) { return; }
+
+        _textUi.text = "";
+        _interval = _speed / _message.Length;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_textUi is null || _message is null || _currentIndex + 1 >= _message.Length) { return; }
+
+        _elapsed += Time.deltaTime;
+        if (_elapsed > _interval)
+        {
+            _elapsed = 0;
+            _currentIndex++;
+            _textUi.text += _message[_currentIndex];
+        }
     }
 }
