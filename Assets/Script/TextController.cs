b3abeec0b1e7c10042956e7db81c579237fd0bd1
ueno-000 <sarasa4869@zoho.com>
2022-11-08 12:52:37 +0900

    [Update] 短く書き換えた

--- Assets/Script/TextController.cs
+++ Assets/Script/TextController.cs
@@ -5,14 +5,12 @@ using UnityEngine.UI;
 
 public class TextController : MonoBehaviour
 {
-    [SerializeField] Text _name;
     [SerializeField] Text  _tex;
     [SerializeField] string[] _text;
 
     // Start is called before the first frame update
     void Start()
     {
-        _name = _name.gameObject.GetComponent<Text>();
         _tex = _tex.gameObject.GetComponent<Text>();
         StartCoroutine("TextCOn");
     }
@@ -24,31 +22,11 @@ public class TextController : MonoBehaviour
 
     public IEnumerator TextCOn()
     {
-        _name.text = "ŸÓ“×";
-        _tex.text = _text[0];
-        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
-        yield return null;
-        _name.text = "‰´";
-        _tex.text = _text[1];
-        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
-        yield return null;
-        _name.text = "’jŽq‚Z¶C";
-        _tex.text = _text[2];
-        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
-        yield return null;
-        _name.text = "’jŽq‚Z¶C";
-        _tex.text = _text[3];
-        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
-        yield return null;
-        _name.text = "‰´";
-        _tex.text = _text[4];
-        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
-        yield return null;
-        _name.text = "ŸÓ“×";
-        _tex.text = _text[5];
-        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
-        yield return null;
-        yield return new WaitForSeconds(1);
-        _tex.transform.parent.gameObject.SetActive(false);
+        foreach (var i in _text)
+        {
+            _tex.text = i.ToString();
+            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        }
+
     }
 }
